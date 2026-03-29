5cdee6f4271b6c02e41653beb0fc85db123ffab2
BOJJA-VARUN <165771890+BOJJA-VARUN@users.noreply.github.com>
2026-03-30 00:26:38 +0530

    Update app.py

--- app.py
+++ app.py
@@ -953,4 +953,5 @@ if BackgroundScheduler is not None and not app.debug:
 
 
 if __name__ == "__main__":
-    app.run(debug=True, host="0.0.0.0", port=5000)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
